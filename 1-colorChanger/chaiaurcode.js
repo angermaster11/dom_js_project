fcff4403eb16c126afc0c0004df947514c934f7d
angermaster11 <krishnabhakt677@gmail.com>
2025-04-10 11:42:23 -0700

    project 1

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -1,6 +1,10 @@
-// const button = document.querySelectorAll("button");
-// const body = document.body;
-
-// // button.forEach((btn)=>{
-// //     console.log("Hello")
-// // })
+const buttons = document.querySelectorAll(".button");
+const body = document.body;
+console.log(buttons)
+buttons.forEach((button)=>{
+    console.log(button.id);
+    button.addEventListener('click',(e)=>{
+        console.log(e.target.id);
+        body.style.backgroundColor = e.target.id;
+    })
+});
